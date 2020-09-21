f4e39174ec23d52ae8d4ce7f47ffd1a99e88c185
jason06286 <dj4871114@gmail.com>
2020-09-21 12:06:38 +0800

    JS 功能一'

---
+++ js/function1.js
@@ -0,0 +1,10 @@
+function multiplication(params) {
+    
+    for (let i = 3; i < 16; i++) {
+        for (let l = 1; l < 16; l++) {
+            let str= `${i}*${l}`
+        console.log(str)
+        }
+    }
+}
+multiplication()
